057fe936875f6dc856fa18abf7e5ec9d76d9c591
Yvan-Nkondog <yvan.nkondog@protonmail.com>
2026-07-22 15:04:46 -0400

    Completion of pset0, views.db, exercises 1, 2, and 3.

---
+++ problem_sets/pset0/views/views/1.sql
@@ -0,0 +1,9 @@
+-- Write a SQL query that a translator might take interest in: list, 
+-- side by side, the Japanese title and the English title for each 
+-- print. Ensure the Japanese title is the first column, followed 
+-- by the English title.
+
+-- Uses views.db
+
+SELECT "japanese_title", "english_title" 
+FROM "views";
---
+++ problem_sets/pset0/views/views/2.sql
@@ -0,0 +1,10 @@
+-- Write a SQL query to list the average colors of prints 
+-- by Hokusai that include “river” in the English title. 
+-- (As an aside, do they have any hint of blue?)
+
+-- Uses views.db
+
+SELECT "average_color"
+FROM "views"
+WHERE "artist" = 'Hokusai'
+AND "english_title" LIKE '%river%';
