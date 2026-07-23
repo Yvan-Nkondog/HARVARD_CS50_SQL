8d9ba7e031701c288815aeba3318500c11a71df2
Yvan-Nkondog <yvan.nkondog@protonmail.com>
2026-07-23 05:45:08 -0400

    Pset0, database players, exercise6.

--- problem_sets/pset0/players/6.sql
+++ problem_sets/pset0/players/6.sql
@@ -5,3 +5,8 @@
 -- alphabetically by first name, followed by last name.
 
 -- Uses players.db
+
+SELECT "first_name", "last_name", "debut"
+FROM "players"
+WHERE "birth_city" = 'Pittsburgh' AND "birth_state" = 'PA'
+ORDER BY "debut" DESC, "first_name" ASC, "last_name" ASC;
