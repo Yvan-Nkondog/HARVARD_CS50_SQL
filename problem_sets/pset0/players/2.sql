077b477f035e4dba2a6f0edc8758f18e4c61b47e
Yvan-Nkondog <yvan.nkondog@protonmail.com>
2026-07-23 05:13:37 -0400

    Pset0, database players, ex2, ex3.

--- problem_sets/pset0/players/2.sql
+++ problem_sets/pset0/players/2.sql
@@ -2,3 +2,7 @@
 -- (e.g., right or left) Babe Ruth hit.
 
 -- Uses players.db
+
+SELECT "bats"
+FROM "players"
+WHERE "first_name" = 'Babe' AND "last_name" = 'Ruth';
--- problem_sets/pset0/players/3.sql
+++ problem_sets/pset0/players/3.sql
@@ -2,3 +2,7 @@
 -- for which a value in the column debut is missing.
 
 -- Uses players.db
+
+SELECT "id"
+FROM "players"
+WHERE "debut" IS NULL
