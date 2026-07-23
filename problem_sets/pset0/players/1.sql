7fe1d3350ef18c61dfe539b7dda0a6d5a1a42813
Yvan-Nkondog <yvan.nkondog@protonmail.com>
2026-07-23 05:02:22 -0400

    Pset0, database players, exercise1.

--- problem_sets/pset0/players/1.sql
+++ problem_sets/pset0/players/1.sql
@@ -2,3 +2,7 @@
 -- (including city, state, and country) of Jackie Robinson.
 
 -- Uses players.db
+
+SELECT "birth_city", "birth_state", "birth_country"
+FROM "players"
+WHERE "first_name" = 'Jackie' AND "last_name" = 'Robinson';
