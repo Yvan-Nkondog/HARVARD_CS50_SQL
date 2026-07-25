9d29871a2b3bf6ecbddcfb71c705b6bdb4de2c05
Yvan-Nkondog <yvan.nkondog@protonmail.com>
2026-07-25 18:54:59 -0400

    Questions 2 to 6, questions only.

---
+++ problem_sets/pset1/moneyball/5.sql
@@ -0,0 +1,9 @@
+-- It’s a bit of a slow day in the office. Though Satchel no longer 
+-- plays, in 5.sql, write a SQL query to find all teams 
+-- that Satchel Paige played for.
+
+-- Your query should return a table with a single column, 
+-- one for the name of the teams.
+
+-- Uses moneyball.db
+
