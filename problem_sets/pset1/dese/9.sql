cf0f294aee325dfcc55001fdad2d0e44ef48b9af
Yvan-Nkondog <yvan.nkondog@protonmail.com>
2026-07-25 10:01:26 -0400

    Pset0, DESE database, ex9.sql, complted.

--- problem_sets/pset1/dese/9.sql
+++ problem_sets/pset1/dese/9.sql
@@ -1,4 +1,4 @@
--- Another parent wants to send their child to a 
+-- Another pair of parents want to send their child to a 
 -- district with few other students. In 9.sql, write 
 -- a SQL query to find the name (or names) of the school
 -- district(s) with the single least number of pupils. 
@@ -6,4 +6,14 @@
 
 -- Uses dese.db
 
+SELECT "name"
+FROM (
+    SELECT *
+    FROM "districts"
+    JOIN "expenditures" ON "expenditures"."district_id" = "districts"."id"
+)
+WHERE "pupils" = (
+    SELECT MIN("pupils")
+    FROM "expenditures"
+);
 
