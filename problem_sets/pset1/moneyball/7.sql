452e4197ec8e322f5bf3f77d619692868f1ff42b
Yvan-Nkondog <yvan.nkondog@protonmail.com>
2026-07-26 03:03:59 -0400

    Pset2, Moneyball database, ex7.

--- problem_sets/pset1/moneyball/7.sql
+++ problem_sets/pset1/moneyball/7.sql
@@ -8,4 +8,13 @@
 
 -- Uses moneyball.db
 
-
+SELECT "first_name", "last_name"
+FROM (
+    SELECT *
+    FROM "players"
+    JOIN "salaries" ON "salaries"."player_id" = "players"."id"
+)
+WHERE "salary" = (
+    SELECT MAX("salary")
+    FROM "salaries"
+);
