36076704d1ad04044ad5cfcd71be47e70ec54f44
Yvan-Nkondog <yvan.nkondog@protonmail.com>
2026-07-26 03:55:01 -0400

    Pset2, Moneyball database, ex8.

--- problem_sets/pset1/moneyball/8.sql
+++ problem_sets/pset1/moneyball/8.sql
@@ -1,8 +1,23 @@
 -- How much would the A’s need to pay to get the best home 
 -- run hitter this past season? In 8.sql, write a SQL query 
--- to find the 2001 salary of the player who hit the most home runs in 2001.
+-- to find the 2001 salary of the player who hit the most 
+-- home runs in 2001.
 
---     Your query should return a table with one column, the salary of the player.
+--     Your query should return a table with one column, the 
+--     salary of the player.
 
 -- Uses moneyball.db
 
+SELECT "salary"
+FROM (
+    SELECT *
+    FROM "salaries"
+    JOIN "performances" ON "performances"."player_id" = "salaries"."player_id"
+)
+WHERE "HR" = (
+    SELECT MAX("HR")
+    FROM "performances"
+    GROUP BY "year"
+    HAVING "year" = 2001
+)
+AND "year" = 2001;
