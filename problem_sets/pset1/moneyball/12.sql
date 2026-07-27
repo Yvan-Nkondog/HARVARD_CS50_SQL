935f8470fbf61e36fdc032358c0334ef3ee53e16
Yvan-Nkondog <yvan.nkondog@protonmail.com>
2026-07-26 23:51:03 -0400

    stete of progress, ex12, incomplete.

--- problem_sets/pset1/moneyball/12.sql
+++ problem_sets/pset1/moneyball/12.sql
@@ -15,4 +15,28 @@
 
 -- Uses moneyball.db
 
+SELECT * FROM "performances" LIMIT 1;
+SELECT * FROM "players" LIMIT 1;
+SELECT * FROM "salaries" LIMIT 1;
+SELECT * FROM "teams" LIMIT 1;
 
+SELECT
+   "first_name",
+   "last_name",
+   CASE
+      WHEN "H" != 0 THEN ROUND ("salary" * 1.0 / "H", 2)
+      ELSE NULL
+    END AS "dollars per hit" 
+    CASE
+      WHEN "RBI" != 0 THEN ROUND ("salary" * 1.0 / "RBI", 2)
+      ELSE NULL
+    END AS "dollars per RBI" 
+FROM "players"
+JOIN "performances" ON "players"."id" = "performances"."player_id"
+JOIN "salaries" ON "salaries"."player_id" = "performances"."player_id"
+AND "salaries"."year" = "performances"."year"
+WHERE "H" != 0 
+WHERE "RBI" != 0 
+AND "salaries"."year" = 2001
+ORDER BY "dollars per hit" ASC, "first_name" ASC, "last_name" ASC
+LIMIT 10;
