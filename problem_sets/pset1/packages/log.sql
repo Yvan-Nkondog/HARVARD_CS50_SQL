b9bd6aa90f889feb8e58386d393d3830004c7582
Yvan-Nkondog <yvan.nkondog@protonmail.com>
2026-07-30 21:19:07 -0400

    Pset1,  packages database, ex1 and ex2.

--- problem_sets/pset1/packages/log.sql
+++ problem_sets/pset1/packages/log.sql
@@ -64,10 +64,57 @@ WHERE "package_id" = 384;
 -- │ 94 │         1 │        384 │        854 │ Drop   │ 2023-07-11 23:07:04.432178 │
 -- ╰────┴───────────┴────────────┴────────────┴────────┴────────────────────────────╯
 
+-- Conclusion :
 -- The package has been picked from the correct address and dropped at the correct
 -- destination.
 
+
+
 -- *** The Devious Delivery ***
+
+-- One important information mentionned is that the
+-- "from_address_id" IS NULL. Hence, I have started by
+-- filtering the packages to keep only those having
+-- "from_address_id" = NULL.
+SELECT *
+FROM "packages"
+WHERE "from_address_id" IS NULL;
+-- Output :
+-- ╭──────┬───────────────┬─────────────────┬───────────────╮
+-- │  id  │   contents    │ from_address_id │ to_address_id │
+-- ╞══════╪═══════════════╪═════════════════╪═══════════════╡
+-- │ 5098 │ Duck debugger │ NULL            │            50 │
+-- ╰──────┴───────────────┴─────────────────┴───────────────╯
+
+
+-- From scans, track the package using the package ID.
+SELECT *
+FROM "scans"
+WHERE "package_id" = 5098;
+-- ╭───────┬───────────┬────────────┬────────────┬────────┬────────────────────────────╮
+-- │  id   │ driver_id │ package_id │ address_id │ action │         timestamp          │
+-- ╞═══════╪═══════════╪════════════╪════════════╪════════╪════════════════════════════╡
+-- │ 30123 │        10 │       5098 │         50 │ Pick   │ 2023-10-24 08:40:16.246648 │
+-- │ 30140 │        10 │       5098 │        348 │ Drop   │ 2023-10-24 10:08:55.610754 │
+-- ╰───────┴───────────┴────────────┴────────────┴────────┴────────────────────────────╯
+
+SELECT *
+FROM "addresses"
+WHERE "id" = 50 OR "id" = 348;
+-- Output :
+-- ╭─────┬───────────────────┬────────────────╮
+-- │ id  │      address      │      type      │
+-- ╞═════╪═══════════════════╪════════════════╡
+-- │  50 │ 123 Sesame Street │ Residential    │
+-- │ 348 │ 7 Humboldt Place  │ Police Station │
+-- ╰─────┴───────────────────┴────────────────╯
+
+-- According to the time stamp, from the "scans" table, the package has
+-- been dropped at address with id 348. Using the "addresses" table,
+-- I have confirmed that the address "type" is a "Police Station", located
+-- at (address) 7 Humboldt Place. 
+-- According to the "scans" table, the package has left from address_id
+-- 50, which corresponds to 123 Sesame Street, a "Residential" address.
 
 -- *** The Forgotten Gift ***
 
