1df519fb15968389d49cd12fc51f7d3c9365b738
Yvan-Nkondog <yvan.nkondog@protonmail.com>
2026-08-04 15:47:27 -0400

    Pset5, snap database, ex1 and ex2 completed.

--- problem_sets/pset5/snap/1.sql
+++ problem_sets/pset5/snap/1.sql
@@ -1,5 +1,29 @@
+-- The app’s user engagement team needs to identify active 
+-- users. Find all usernames of users who have logged in since 
+-- 2024-01-01. Ensure your query uses the search_users_by_last_login index, 
+-- which is defined as follows:
+
+--      CREATE INDEX "search_users_by_last_login"
+--      ON "users"("last_login_date");
+
+-- Uses snap.db
 
 
-SELECT * FROM "friends" LIMIT 1;
-SELECT * FROM "messages" LIMIT 1;
-SELECT * FROM "users" LIMIT 1;
+-- Delete existing index.
+DROP INDEX IF EXISTS "search_users_by_last_login";
+
+-- Create the index mentioned in the question.
+CREATE INDEX "search_users_by_last_login"
+ON "users"("last_login_date");
+
+-- Execute the query
+SELECT "username" 
+FROM "users"
+WHERE "last_login_date" > date('2024-01-01');
+
+-- Explain the query plan
+EXPLAIN QUERY PLAN
+SELECT "username" 
+FROM "users"
+WHERE "last_login_date" > date('2024-01-01');
+
