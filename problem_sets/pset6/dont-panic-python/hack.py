d05f2350229c0803315d0536d6026e83a3244b88
Yvan-Nkondog <yvan.nkondog@protonmail.com>
2026-08-06 20:20:49 -0400

    Pset6, exercise dont-panic-python, completed.

--- problem_sets/pset6/dont-panic-python/hack.py
+++ problem_sets/pset6/dont-panic-python/hack.py
@@ -1,1 +1,21 @@
-print("Hacked!")
+from cs50 import SQL
+
+# Establish the connection to the dont-panic database.
+db = SQL("sqlite:///dont-panic.db")
+
+# Create a variable to store the user defined password.
+password = input("Please, enter a password: ")
+
+# Execute sql statement with Python, ensuring to use the
+# '?', from the cs50's library, as placeholder (prepared statement),
+# for a value to supply later.
+db.execute(
+    """
+    UPDATE "users"
+    SET "password" = ?
+    WHERE "username" = 'admin';
+    """,
+    # Tell the execute statement which value to replace
+    # the question mark with.
+    password
+)
