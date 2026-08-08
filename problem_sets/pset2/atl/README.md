f11c9bab3ee21ecf4769d73f593f23d46f4965ab
Yvan-Nkondog <yvan.nkondog@protonmail.com>
2026-08-08 08:14:23 -0400

    Pset2, 'atl database' exercise, completed and tested.

--- problem_sets/pset2/atl/README.md
+++ problem_sets/pset2/atl/README.md
@@ -1,3 +1,6 @@
 ## Access exercise description through the following link:
 
 https://cs50.harvard.edu/sql/psets/2/atl/
+
+
+### Run sqlite3 atl.db to create the database atl.db.
--- problem_sets/pset2/atl/schema_test.sql
+++ problem_sets/pset2/atl/schema_test.sql
@@ -70,3 +70,80 @@ CREATE TABLE "flights" (
     FOREIGN KEY("airline_id") REFERENCES "airlines"("id") ON DELETE CASCADE,
     CHECK ("arrival_datetime" > "departure_datetime")
 );
+
+
+-- Insert values into the tables.
+INSERT INTO "passengers" ("first_name", "last_name", "age")
+VALUES 
+('Amelia', 'Earhart', 39),
+('John', 'Doe', 18),
+('Jane', '', 5);
+
+SELECT * FROM "passengers";
+
+
+-- Insert into airlines
+INSERT INTO "airlines"("name")
+VALUES
+('DELTA'),
+('AIR CANADA'),
+('AIR TRANSAT'),
+('AIR FRANCE'),
+('BRUXELLE AIRWAYS');
+
+-- Insert into concourse (table filled once globally,
+-- except if there are new concourses to add later).
+INSERT INTO "concourses"("concourse")
+VALUES
+('A'), ('B'), ('C'), ('D'), ('E'), ('F'), ('T'); 
+
+-- Link the airlines to the concourses.
+INSERT INTO "airline_concourses"("airline_id", "concourse_id")
+VALUES
+-- 'DELTA'
+(1, 1), (1, 2), (1, 3), (1, 4), (1, 7),
+-- 'AIR CANADA'
+(2, 1), (2, 2), (2, 3), (2, 4), (2, 7),
+-- 'AIR TRANSAT'
+(3, 5), (3, 6), (3, 7),
+-- 'AIR FRANCE'
+(4, 2), (4, 3), (4, 5), (4, 7),
+-- 'BRUXELLE'S AIRWAYS'
+(5, 1), (5, 5), (5, 6), (5, 7);
+
+-- Display the tables.
+SELECT * FROM "airlines";
+SELECT * FROM "concourses";
+SELECT * FROM "airline_concourses";
+
+SELECT *
+FROM "airlines"
+JOIN "airline_concourses" ON "airline_concourses"."airline_id" = "airlines"."id"
+JOIN "concourses" ON "concourses"."id" = "airline_concourses"."concourse_id"
+WHERE "airlines"."name" = 'DELTA';
+
+
+-- Insert data into "flights" table.
+INSERT INTO "flights"("airline_id", "flight_number", "departing_airport", 
+                    "heading_airport", "departure_datetime", "arrival_datetime")
+VALUES
+(1, 300, 'ATL', 'BOS', '2023-08-03 18:46', '2023-08-03 21:09'),
+(2, 500, 'YUL', 'ATL', '2024-07-02 11:17', '2024-07-03 14:02');
+
+-- Display the table.
+SELECT * FROM "flights";
+
+
+-- Insert into the checkins table
+INSERT INTO "checkins" ("passenger_id", "flight_id", "checkin_datetime")
+VALUES
+(1, 1, '2023-08-03 15:03');
+
+INSERT INTO "checkins" ("passenger_id", "flight_id")
+VALUES
+(1, 1),
+(2, 2),
+(3, 1);
+
+-- Display the table.
+SELECT * FROM "checkins";
